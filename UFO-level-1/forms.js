e69b019b0f68d6ad6092c3129e4a4703b3a9354d
ASMVM <66266053+asmvm@users.noreply.github.com>
2020-10-20 11:51:16 -0400

    final js code, filter for state and city added

--- UFO-level-1/forms.js
+++ UFO-level-1/forms.js
@@ -1,19 +1,35 @@
 
 // Assign the data from `data.js` to a descriptive variable
 var sightings = data;
+
+
 // Select the button
-var button = d3.select("#datebutton");
+var button = d3.select("#filterbutton");
 var resetButton = d3.select(".btn-outline-success")
+
+
 // Select the form
 var form = d3.select("#form");
-// Create event handlers 
-button.on("click", runEnter);
-form.on("submit",runEnter);
+
+// Create event handlers for: 
+//datetime filter
+button.on("click", runDatetime);
+form.on("submit",runDatetime);
+
+
+//city filter
+button.on("click", runCity);
+form.on("submit",runCity);
+
+//state filter
+button.on("click", runState);
+form.on("submit",runState);
+
+//reset filter
 resetButton.on("click", reset);
 
-
 // Complete the event handler function for the form
-function runEnter() {  
+function runDatetime() {  
   //Clear the table when running function
   tbody.html("");   
   // Prevent the page from refreshing
@@ -22,30 +38,56 @@ function runEnter() {
   var inputElement = d3.select("#formGroupExampleInput");
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
-
   // Print to console and verify date is selected
   console.log(inputValue); 
-
-  if (inputValue > sightings.datetime){
-    function alertmsg() {alert("Oops!");}
-  }
-
   // Create filter for user input 
   var filteredData = sightings.filter(sight => sight.datetime === inputValue);
   // Record filter is working properly in console
   console.log(filteredData);
-
   // Use same function from app.js to build table and apply to filteredData
-  filteredData.forEach(UFOsightings);
-  
-  
+  filteredData.forEach(UFOsightings);  
 };
 
-function reset() {
-  
-  tbody.html("");
+// function runCity() {  
+//   //Clear the table when running function
+//   tbody.html("");   
+//   // Prevent the page from refreshing
+//   d3.event.preventDefault();  
+//   // Select the input element and get the raw HTML node
+//   var inputElement = d3.select("#formGroupExampleInput");
+//   // Get the value property of the input element
+//   var inputValue = inputElement.property("value").toLowerCase();
+//   // Print to console and verify date is selected
+//   console.log(inputValue); 
+//   // Create filter for user input 
+//   var filteredData = sightings.filter(sight => sight.city === inputValue);
+//   // Record filter is working properly in console
+//   console.log(filteredData);
+//   // Use same function from app.js to build table and apply to filteredData
+//   filteredData.forEach(UFOsightings);  
+// };
 
+// function runState() {  
+//   //Clear the table when running function
+//   tbody.html("");   
+//   // Prevent the page from refreshing
+//   d3.event.preventDefault();  
+//   // Select the input element and get the raw HTML node
+//   var inputElement = d3.select("#formGroupExampleInput");
+//   // Get the value property of the input element
+//   var inputValue = inputElement.property("value").toLowerCase();
+//   // Print to console and verify date is selected
+//   console.log(inputValue); 
+//   // Create filter for user input 
+//   var filteredData = sightings.filter(sight => sight.state === inputValue);
+//   // Record filter is working properly in console
+//   console.log(filteredData);
+//   // Use same function from app.js to build table and apply to filteredData
+//   filteredData.forEach(UFOsightings);  
+// };
+
+function reset() {
+  tbody.html("");
   d3.event.preventDefault();
-  
   data.forEach(UFOsightings);
 };
